22432305c8092b262fb04eb0cf81ca2f06a62052
David9191 <jisung9105@gmail.com>
2023-08-08 23:57:15 +0900

    Feat: 기본 라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+import './index.css';
+import Todo from './todo/Todo';
+import SignUp from './signUp/SignUp';
+import SignIn from './signIn/SignIn';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SignIn />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'signup',
+    element: <SignUp />,
+  },
+  {
+    path: 'signin',
+    element: <SignIn />,
+  },
+  {
+    path: 'todo',
+    element: <Todo />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
