c2dadacfe983c65054f84dde115866df124ab721
David9191 <jisung9105@gmail.com>
2023-08-11 02:35:28 +0900

    Feat: 로그인 구현 완료

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import SignIn from '../signIn/components/SignIn';
+
+const SignInPage = () => {
+  return (
+    <>
+      <Header />
+      <SignIn />
+    </>
+  );
+};
+
+export default SignInPage;
