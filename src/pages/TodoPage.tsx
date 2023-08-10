df8de729488d1e637fa6ad678867b4b19d00126e
David9191 <jisung9105@gmail.com>
2023-08-11 03:15:48 +0900

    Add: Todo page & component 생성

---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Todo from '../todo/components/Todo';
+
+const TodoPage = () => {
+  return (
+    <>
+      <Header />
+      <Todo />
+    </>
+  );
+};
+
+export default TodoPage;
