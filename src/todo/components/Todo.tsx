f089604c20732b39628f8700107e4eeec731e0ce
David9191 <jisung9105@gmail.com>
2023-08-11 13:31:03 +0900

    Feat: 토큰 없을 시 로그인 페이지로 이동하는 기능 구현

--- src/todo/components/Todo.tsx
+++ src/todo/components/Todo.tsx
@@ -1,5 +1,29 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Todo = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      alert('로그인된 유저만 사용할 수 있습니다.');
+      navigate('/signin');
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  );
 };
 
 export default Todo;
